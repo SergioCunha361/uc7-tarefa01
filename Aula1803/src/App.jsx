7a8f470387a155050025b66cd063720dc0f642bd
SergioCunha361 <sergiocunha50@gmail.com>
2025-03-19 11:50:30 -0300

    autalizado em casa dia 19-03

--- Aula1803/src/App.jsx
+++ Aula1803/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter,Routes, Route } from "react-router-dom";
+import { BrowserRouter,Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import BandDetalhes from "./pages/BandDetalhes/BandDetalhes";
 
@@ -9,10 +9,12 @@ function App() {
        <BrowserRouter>
        <Routes>
               <Route path="/" element={<Home />}/>
-              <Route path="/band/:id" element={<BandDetalhes />}/>
+              <Route path="/band/:id" element={<BandDetalhes />}/>          
 
-       </Routes>       
-       </BrowserRouter>
+       </Routes> 
+       {/* Link estilizado como botão */}
+      <Link to="/" className="btn-retornar">Retornar</Link>      
+      </BrowserRouter>
  ) 
 }
 
